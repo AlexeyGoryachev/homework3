0122e944cb28e8f7d657b8a642f2bd7d3ad03ac7
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-02-15 03:04:21 +0300

    This is where I start doing my homework.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1");
-
+        byte cats = 127;
+        short owls = 32767;
+        int dogs = 2147483647;
+        long humans = 999999999999L;
+        float x = 20.6666666f;
+        double y = 20.6666666666;
+        System.out.println("Значение переменной с типом byte = " + cats);
+        System.out.println("Значение переменной с типом short = " + owls);
+        System.out.println("Значение переменной с типом int = " + dogs);
+        System.out.println("Значение переменной с типом long = " + humans);
+        System.out.println("Значение переменной с типом float = " + x);
+        System.out.println("Значение переменной с типом double = " + y);
     }
 }
